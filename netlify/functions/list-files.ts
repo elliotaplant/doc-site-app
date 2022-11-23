c2533d5295975d8b6e3735dc6b79f9dd89dc5bdb
Elliot Plant <elliotaplant@gmail.com>
2022-11-23 13:50:07 -0800

    Send files up

--- netlify/functions/list-files.ts
+++ netlify/functions/list-files.ts
@@ -7,7 +7,6 @@ import { Credentials } from 'google-auth-library';
 const handler: Handler = async () => {
   const tokensResponse = await fetchBackend('/drive-tokens');
   const tokens: Credentials = (await tokensResponse.json()) as any;
-  console.log('tokens', tokens);
 
   // // Example of using Google Drive API to list filenames in user's Drive.
   const drive = google.drive('v3');
@@ -17,59 +16,25 @@ const handler: Handler = async () => {
     process.env.REDIRECT_URL
   );
 
-  console.log('alpha');
   oauth2Client.setCredentials(tokens);
 
-  // const response = await drive.files.list({
-  //   // q: "mimeType = 'application/vnd.google-apps.folder'",
-  //   q: "parents in 'root' and mimeType = 'application/vnd.google-apps.folder'",
-  //   auth: oauth2Client,
-  //   pageSize: 10,
-  //   // fields: "nextPageToken, files(id, name)",
-  // });
-
-  // console.log('response.data', response.data);
-  // const files = response.data.files;
-  // if (files.length) {
-  //   console.log('Files:');
-  //   files.map((file) => {
-  //     console.log(`${file.name} (${file.id})`);
-  //   });
-  // } else {
-  //   console.log('No files found.');
-  // }
-  await new Promise((resolve, reject) => {
-    console.log('beta');
-    drive.files.list(
-      {
-        // q: "mimeType = 'application/vnd.google-apps.folder'",
-        q: "parents in 'root' and mimeType = 'application/vnd.google-apps.folder'",
-        auth: oauth2Client,
-        pageSize: 10,
-        // fields: "nextPageToken, files(id, name)",
-      },
-      (err, res) => {
-        console.log('gamma');
-        if (err) {
-          reject(err);
-        }
-
-        console.log('res.data', res.data);
-        const files = res.data.files;
-        if (files.length) {
-          console.log('Files:');
-          files.map((file) => {
-            console.log(`${file.name} (${file.id})`);
-          });
-        } else {
-          console.log('No files found.');
-        }
-        resolve(null);
-      }
-    );
+  const response = await drive.files.list({
+    // q: "mimeType = 'application/vnd.google-apps.folder'",
+    q: "parents in 'root' and mimeType = 'application/vnd.google-apps.folder'",
+    auth: oauth2Client,
+    pageSize: 10,
+    // fields: "nextPageToken, files(id, name)",
   });
 
-  return { statusCode: 200 };
+  const files = response.data.files;
+  if (files?.length) {
+    return {
+      body: JSON.stringify(response.data),
+      statusCode: 200,
+    };
+  }
+
+  return { statusCode: 400 };
 };
 
 export { handler };
