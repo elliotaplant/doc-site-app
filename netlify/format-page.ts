1ba21b765bf7d83e06edbace2342f4148b758f16
Elliot Plant <elliotaplant@gmail.com>
2022-11-23 15:07:50 -0800

    Add formatting to one page

--- netlify/format-page.ts
+++ netlify/format-page.ts
@@ -1,11 +1,58 @@
 import { Readable } from 'stream';
-import { parse } from 'node-html-parser';
+import { parse, HTMLElement } from 'node-html-parser';
 
 export async function formatPage(rawPage: Readable) {
   const pageAsString = await streamToString(rawPage);
   const root = parse(pageAsString);
 
+  const headNode = root.querySelector('head');
+  const metaElement = element('meta', {
+    name: 'viewport',
+    content: 'width=device-width',
+  });
+
+  const styleElement = element(
+    'style',
+    {},
+    `
+    body { 
+      margin: 0 auto; 
+    }
+
+    @media (max-width: 900px) {
+      body.doc-content {
+        padding: 1em !important;
+      }
+    }
+  `
+  );
+  headNode?.appendChild(metaElement);
+  headNode?.appendChild(styleElement);
+
   return root.toString();
+}
+
+function element(
+  tagName: string,
+  attrs: { [key: string]: string },
+  contents?: string
+) {
+  const element = new HTMLElement(
+    tagName,
+    {},
+    attrs &&
+      Object.entries(attrs)
+        .map(([key, value]) => `${key}=${value}`)
+        .join(' '),
+    null,
+    [0, 0]
+  );
+
+  if (contents) {
+    element.textContent = contents;
+  }
+
+  return element;
 }
 
 async function streamToString(stream: Readable) {
