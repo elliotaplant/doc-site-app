9ad556cfd0500399f54535b24ce349b81bae385a
Elliot Plant <elliotaplant@gmail.com>
2023-01-08 13:30:53 -0800

    Make account page cute

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -14,11 +14,19 @@ export function AccountPage() {
   return (
     <>
       <PageTitle>Account</PageTitle>
-      <h3>Hello {JSON.stringify(user!.email)}</h3>
-      <button style={{ width: 100 }} onClick={connectGoogleDrive}>
+      <h3 className="font-semibold">{user!.email}</h3>
+      <button
+        className="inline-flex h-full w-full max-w-sm cursor-pointer items-center justify-center rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 text-sm font-semibold text-text shadow-sm hover:text-heading focus:text-heading focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-text dark:focus:ring-white/80"
+        onClick={connectGoogleDrive}
+      >
         Connect to Google Drive
       </button>
-      <button onClick={() => logoutUser()}>Log out</button>
+      <button
+        className="inline-flex h-full w-full max-w-sm cursor-pointer items-center justify-center rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 text-sm font-semibold text-text shadow-sm hover:text-heading focus:text-heading focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-text dark:focus:ring-white/80"
+        onClick={() => logoutUser()}
+      >
+        Log out
+      </button>
     </>
   );
 }
