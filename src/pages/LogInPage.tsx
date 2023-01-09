f1aede9f152dbb1e3937555b5aa9c0344cc74d04
Elliot Plant <elliotaplant@gmail.com>
2023-01-08 22:15:15 -0800

    A bit more styling

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -6,36 +6,23 @@ import { useIdentityContext } from 'react-netlify-identity';
 export function LogInPage() {
   const { loginUser } = useIdentityContext();
   const [error, setError] = useState('');
-  const emailInput = useRef<HTMLInputElement>(null!);
-  const passwordInput = useRef<HTMLInputElement>(null!);
-  const logInButton = useRef<HTMLButtonElement>(null!);
+  const [submitting, setSubmitting] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassWord] = useState('');
 
-  useEffect(() => {
-    logInButton.current.disabled = true;
-  }, [emailInput, passwordInput]);
-
-  const handleChange = (): void => {
-    const email = emailInput.current.value;
-    const password = passwordInput.current.value;
-    if (email && password) {
-      logInButton.current.disabled = false;
-    } else {
-      logInButton.current.disabled = true;
-    }
-  };
-
-  const logIn = (event: React.FormEvent<HTMLFormElement>): void => {
+  const logIn = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const email = emailInput.current.value;
-    const password = passwordInput.current.value;
-    loginUser(email, password, true).catch((error) => {
-      if (error?.json?.error_description) {
-        setError(error.json.error_description);
-      } else {
-        setError('Failed to log in');
-      }
-      console.log(error);
-    });
+    setSubmitting(true);
+    loginUser(email, password, true)
+      .catch((error) => {
+        if (error?.json?.error_description) {
+          setError(error.json.error_description);
+        } else {
+          setError('Failed to log in');
+        }
+        console.log(error);
+      })
+      .finally(() => setSubmitting(false));
   };
 
   return (
@@ -49,30 +36,36 @@ export function LogInPage() {
       }}
     >
       <h1>Sign In</h1>
-      <form onSubmit={logIn}>
-        <label style={{ display: 'block' }} htmlFor="email">
+      <form onSubmit={logIn} className="w-full max-w-sm">
+        <label htmlFor="email" className="block text-sm font-semibold text-heading">
           Email
         </label>
         <input
-          style={{ display: 'block' }}
-          type="email"
           id="email"
-          ref={emailInput}
-          onChange={handleChange}
+          name="email"
+          type="email"
+          placeholder="you@email.com"
+          className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
+          onChange={(e) => setEmail(e.currentTarget.value)}
         />
-        <label style={{ display: 'block' }} htmlFor="password">
+
+        <label htmlFor="password" className="mt-4 block text-sm font-semibold text-heading">
           Password
         </label>
         <input
-          style={{ display: 'block' }}
-          type="password"
           id="password"
-          ref={passwordInput}
-          onChange={handleChange}
+          name="password"
+          type="password"
+          className="mt-2 block w-full rounded-xl border-2 border-muted-3 bg-transparent px-4 py-2.5 font-semibold text-heading placeholder:text-text/50 focus:border-primary focus:outline-none focus:ring-0 sm:text-sm"
+          onChange={(e) => setPassWord(e.currentTarget.value)}
         />
         {error ? <p>{error}</p> : null}
-        <button type="submit" ref={logInButton}>
-          Log in
+        <button
+          type="submit"
+          disabled={submitting}
+          className="w-full mt-6 inline-flex cursor-pointer items-center justify-center rounded-xl border-2 border-primary bg-primary px-4 py-2.5 text-base font-semibold text-white shadow-sm hover:border-primary-accent hover:bg-primary-accent focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:border-primary disabled:hover:bg-primary disabled:hover:text-white dark:focus:ring-white/80"
+        >
+          Sign In
         </button>
       </form>
       <Link to="../create-account">Create Account</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const eggshellDelightsTheme = require('tailwind-saasblocks/themes/eggshell-delights.theme');
+const midnightEnvyTheme = require('tailwind-saasblocks/themes/midnight-envy.theme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('tailwind-children'),
+    require('tailwind-saasblocks')({
+      themes: {
+        dark: midnightEnvyTheme,
+        light: eggshellDelightsTheme,
+      },
+    }),
+  ],
 };
