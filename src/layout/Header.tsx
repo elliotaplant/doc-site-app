0413ce90d9e4133d23ae7b6d2b10efddfacc96d0
Elliot Plant <elliotaplant@gmail.com>
2023-01-09 16:54:10 -0800

    Logo stuff

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
+import { DocSiteLogo } from '../icons';
 
 export function Header() {
   return (
     <header className="relative z-20 flex h-16 flex-1 shrink-0 items-center justify-between bg-layer-2 px-4 shadow sm:px-6">
-      <h1 className="text-3xl font-semibold text-heading">DocSite</h1>
+      <h1 className="text-3xl font-semibold text-heading">
+        <DocSiteLogo />
+        DocSite
+      </h1>
       <nav className="flex space-x-4">
         <Link to="/" className="text-lg font-semibold text-heading">
           Sites
