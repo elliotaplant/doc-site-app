b9ec1ef91702da822bff10c2f2eac36dee77cf5f
Elliot Plant <elliotaplant@gmail.com>
2023-01-09 17:17:11 -0800

    Remove log

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { docSiteLogoSrc } from '../icons';
 
-console.log('DocSiteLogo', docSiteLogoSrc);
 export function Header() {
   return (
     <header className="relative z-20 flex h-16 flex-1 shrink-0 items-center justify-between bg-layer-2 px-4 shadow sm:px-6">
