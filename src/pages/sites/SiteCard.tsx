64ffb8855e2744d2bc53c7d7dc8df8626ac6e101
Elliot Plant <elliotaplant@gmail.com>
2023-01-11 17:36:20 -0800

    Better card styles

--- src/pages/sites/SiteCard.tsx
+++ src/pages/sites/SiteCard.tsx
@@ -12,7 +12,7 @@ export function SiteCard({ project, refreshFile }: SiteCardProps) {
     <li key={project.projectId} className="bg-layer-2 rounded-xl">
       <div className="w-full px-8 py-6">
         <dt className="text-heading text-lg font-semibold">{project.projectId}</dt>
-        <div className="flex flex-row-reverse gap-4">
+        <div className="flex flex-col gap-4 mt-4">
           <button
             className="border-muted-3 text-text hover:text-heading focus:text-heading disabled:hover:text-text inline-flex h-full cursor-pointer items-center justify-center rounded-xl border-2 bg-transparent px-4 py-2.5 text-sm font-semibold shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 dark:focus:ring-white/80"
             onClick={() => refreshFile(project.projectId)}
