02d945db36b08d524e8b32a9a42d8471e36c5734
Elliot Plant <elliotaplant@gmail.com>
2023-01-16 18:19:28 -0800

    Make login page use final form

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -1,30 +1,33 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import { useIdentityContext } from 'react-netlify-identity';
 import { docSiteLogoSrc } from '../icons';
+import { Field, withTypes } from 'react-final-form';
+import { FORM_ERROR } from 'final-form';
+import { TextField } from '../components/TextField';
+import { FormError } from '../components/FormError';
+
+interface CreateAccountFormFields {
+  email: string;
+  password: string;
+}
+
+const { Form } = withTypes<CreateAccountFormFields>();
 
 export function CreateAccountPage() {
   const { loginUser, signupUser } = useIdentityContext();
-  const [error, setError] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassWord] = useState('');
 
-  const signUp = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setSubmitting(true);
-    signupUser(email, password, {})
-      .then(() => loginUser(email, password, true))
-      .catch((error) => {
-        console.error(error);
-        if (error?.json?.error_description) {
-          setError(error.json.error_description);
-        } else {
-          setError('Failed to log in');
-        }
-      })
-      .finally(() => setSubmitting(false));
+  const onSubmit = async ({ email, password }: CreateAccountFormFields) => {
+    try {
+      await signupUser(email, password, {});
+      await loginUser(email, password, true);
+    } catch (error: any) {
+      console.error(error);
+      if (error?.json?.error_description) {
+        return { [FORM_ERROR]: error.json.error_description };
+      } else {
+        return { [FORM_ERROR]: 'Failed to log in' };
+      }
+    }
   };
 
   return (
@@ -33,61 +36,63 @@ export function CreateAccountPage() {
         <img src={docSiteLogoSrc} alt="DocSite Logo" className="inline-block w-8" />
         <span>DocSite</span>
       </h1>
-      <form onSubmit={signUp} className="flex w-full max-w-sm flex-col">
-        <label htmlFor="email" className="text-heading block text-sm font-semibold">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="you@email.com"
-          className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, submitError, submitting }) => (
+          <form onSubmit={handleSubmit} className="flex w-full max-w-sm flex-col gap-4">
+            <Field
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="you@email.com"
+              className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
+              component={TextField}
+            />
 
-        <label htmlFor="password" className="text-heading mt-4 block text-sm font-semibold">
-          Password
-        </label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="············"
-          className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
-          onChange={(e) => setPassWord(e.currentTarget.value)}
-        />
-        {error ? <p>{error}</p> : null}
-        <button
-          type="submit"
-          disabled={submitting}
-          className="border-primary bg-primary hover:border-primary-accent hover:bg-primary-accent disabled:hover:border-primary disabled:hover:bg-primary mt-6 inline-flex w-full cursor-pointer items-center justify-center rounded-xl border-2 px-4 py-2.5 text-base font-semibold text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-white dark:focus:ring-white/80"
-        >
-          Register
-        </button>
-        <p className="text-text mt-6 text-center text-sm">
-          Already have an account?&nbsp;
-          <Link to="../log-in" className="text-primary hover:text-primary-accent font-semibold">
-            Login
-          </Link>
-        </p>
-        <p className="text-text mt-6 text-center text-sm">
-          By signing up, you agree to DocSite's{' '}
-          <a
-            className="text-primary hover:text-primary-accent font-semibold"
-            href="https://docsite.io/privacy"
-          >
-            privacy&nbsp;policy
-          </a>{' '}
-          and{' '}
-          <a
-            className="text-primary hover:text-primary-accent font-semibold"
-            href="https://www.docsite.io/terms-and-conditions"
-          >
-            terms&nbsp;and&nbsp;conditions
-          </a>
-        </p>
-      </form>
+            <Field
+              name="password"
+              type="password"
+              label="Password"
+              placeholder="············"
+              className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
+              component={TextField}
+            />
+
+            {submitError && <FormError>{submitError}</FormError>}
+
+            <button
+              type="submit"
+              disabled={submitting}
+              className="border-primary bg-primary hover:border-primary-accent hover:bg-primary-accent disabled:hover:border-primary disabled:hover:bg-primary mt-6 inline-flex w-full cursor-pointer items-center justify-center rounded-xl border-2 px-4 py-2.5 text-base font-semibold text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-white dark:focus:ring-white/80"
+            >
+              Register
+            </button>
+
+            <p className="text-text mt-6 text-center text-sm">
+              Already have an account?&nbsp;
+              <Link to="../log-in" className="text-primary hover:text-primary-accent font-semibold">
+                Login
+              </Link>
+            </p>
+            <p className="text-text mt-6 text-center text-sm">
+              By signing up, you agree to DocSite's{' '}
+              <a
+                className="text-primary hover:text-primary-accent font-semibold"
+                href="https://docsite.io/privacy"
+              >
+                privacy&nbsp;policy
+              </a>{' '}
+              and{' '}
+              <a
+                className="text-primary hover:text-primary-accent font-semibold"
+                href="https://docsite.io/terms-and-conditions"
+              >
+                terms&nbsp;and&nbsp;conditions
+              </a>
+            </p>
+          </form>
+        )}
+      />
     </div>
   );
 }
--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,29 +1,32 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { useIdentityContext } from 'react-netlify-identity';
 import { docSiteLogoSrc } from '../icons';
+import { Field, withTypes } from 'react-final-form';
+import { FORM_ERROR } from 'final-form';
+import { TextField } from '../components/TextField';
+import { FormError } from '../components/FormError';
+
+interface LoginFormFields {
+  email: string;
+  password: string;
+}
+
+const { Form } = withTypes<LoginFormFields>();
 
 export function LogInPage() {
   const { loginUser } = useIdentityContext();
-  const [error, setError] = useState('');
-  const [submitting, setSubmitting] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassWord] = useState('');
 
-  const logIn = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    setSubmitting(true);
-    loginUser(email, password, true)
-      .catch((error) => {
-        if (error?.json?.error_description) {
-          setError(error.json.error_description);
-        } else {
-          setError('Failed to log in');
-        }
-        console.log(error);
-      })
-      .finally(() => setSubmitting(false));
+  const onSubmit = async ({ email, password }: LoginFormFields) => {
+    try {
+      await loginUser(email, password, true);
+    } catch (e: any) {
+      console.error(e);
+      if (e?.json?.error_description) {
+        return { [FORM_ERROR]: e?.json?.error_description };
+      }
+      return { [FORM_ERROR]: 'Failed to log in' };
+    }
   };
 
   return (
@@ -32,64 +35,50 @@ export function LogInPage() {
         <img src={docSiteLogoSrc} alt="DocSite Logo" className="inline-block w-8" />
         <span>DocSite</span>
       </h1>
-      <form onSubmit={logIn} className="flex w-full max-w-sm flex-col">
-        <label htmlFor="email" className="text-heading block text-sm font-semibold">
-          Email
-        </label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="you@email.com"
-          className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit, submitting, submitError }) => (
+          <form onSubmit={handleSubmit} className="flex w-full max-w-sm flex-col gap-4">
+            <Field
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="you@email.com"
+              className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
+              component={TextField}
+            />
 
-        <label htmlFor="password" className="text-heading mt-4 block text-sm font-semibold">
-          Password
-        </label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="············"
-          className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
-          onChange={(e) => setPassWord(e.currentTarget.value)}
-        />
-        {error ? <p>{error}</p> : null}
-        <button
-          type="submit"
-          disabled={submitting}
-          className="border-primary bg-primary hover:border-primary-accent hover:bg-primary-accent disabled:hover:border-primary disabled:hover:bg-primary mt-6 inline-flex w-full cursor-pointer items-center justify-center rounded-xl border-2 px-4 py-2.5 text-base font-semibold text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-white dark:focus:ring-white/80"
-        >
-          Login
-        </button>
-        <p className="text-text mt-6 text-center text-sm">
-          No account?&nbsp;
-          <Link
-            to="../create-account"
-            className="text-primary hover:text-primary-accent font-semibold"
-          >
-            Sign Up
-          </Link>
-        </p>
-        <p className="text-text mt-6 text-center text-sm">
-          By signing up, you agree to DocSite's{' '}
-          <a
-            className="text-primary hover:text-primary-accent font-semibold"
-            href="https://docsite.io/privacy"
-          >
-            privacy&nbsp;policy
-          </a>{' '}
-          and{' '}
-          <a
-            className="text-primary hover:text-primary-accent font-semibold"
-            href="https://www.docsite.io/terms-and-conditions"
-          >
-            terms&nbsp;and&nbsp;conditions
-          </a>
-        </p>
-      </form>
+            <Field
+              name="password"
+              type="password"
+              label="Password"
+              placeholder="············"
+              className="border-muted-3 text-heading placeholder:text-text/50 focus:border-primary mt-2 block w-full rounded-xl border-2 bg-transparent px-4 py-2.5 font-semibold focus:outline-none focus:ring-0 sm:text-sm"
+              component={TextField}
+            />
+
+            {submitError && <FormError>{submitError}</FormError>}
+
+            <button
+              type="submit"
+              disabled={submitting}
+              className="border-primary bg-primary hover:border-primary-accent hover:bg-primary-accent disabled:hover:border-primary disabled:hover:bg-primary mt-6 inline-flex w-full cursor-pointer items-center justify-center rounded-xl border-2 px-4 py-2.5 text-base font-semibold text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400/80 focus:ring-offset-0 disabled:opacity-30 disabled:hover:text-white dark:focus:ring-white/80"
+            >
+              Login
+            </button>
+
+            <p className="text-text mt-6 text-center text-sm">
+              No account?&nbsp;
+              <Link
+                to="../create-account"
+                className="text-primary hover:text-primary-accent font-semibold"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </form>
+        )}
+      />
     </div>
   );
 }
