b8deec6de4df8f69a5c9666264aa40194517d6b5
Elliot Plant <elliotaplant@gmail.com>
2023-05-22 16:55:34 -0700

    Add another check

--- netlify/functions/is-authed.ts
+++ netlify/functions/is-authed.ts
@@ -1,7 +1,7 @@
 import { Handler } from '@netlify/functions';
 
 const handler: Handler = async (event, context) => {
-  const sub = context.clientContext?.user?.sub;
+  const sub = context?.clientContext?.user?.sub;
 
   return {
     statusCode: 200,
